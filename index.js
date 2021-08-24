61204ba8d9bbeff707c32997723ae8598a3d2a66
Eric <edhavey@homemail.com>
2021-08-24 15:31:38 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+let currentUser = "guy";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
